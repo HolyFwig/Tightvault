14ee8cef0ba8961f59dde302eb33f8dffcb597cd
Amy <maydimmick@gmail.com>
2023-04-12 11:33:58 -0500

    Update index.mjs

--- index.mjs
+++ index.mjs
@@ -4,7 +4,7 @@ import nodeStatic from 'node-static';
 
 
 const bare =  createServer('/bare/');
-const serve = new nodeStatic.Server('static/');
+const serve = new nodeStatic.Server('public/');
 
 const server = http.createServer();
 
